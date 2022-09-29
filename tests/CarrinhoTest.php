2fff02e0758f32ec63bd924ce293c39f05d31705
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-09-29 00:16:03 -0300

    11 - Incrementando Carrinho de Compras com TDD

--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -15,4 +15,25 @@ class CarrinhoTest extends TestCase
 
         $this->assertTrue($classe);
     }
+
+    public function testAdicaoDeProdutosNoCarrinho()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice(19.99);
+        $produto->setSlug('produto-1');
+
+        $produto2 = new Produto();
+        $produto2->setName('Produto 2');
+        $produto2->setPrice(19.99);
+        $produto2->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+        $carrinho->addProduto($produto2);
+
+        $this->assertIsArray($carrinho->getProdutos());
+        $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[0]);
+        $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[1]);
+    }
 }
