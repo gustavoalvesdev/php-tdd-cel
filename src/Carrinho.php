8b3d7d4352a21a8a1b01f98f77ce03946c27184a
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-09-29 00:30:11 -0300

    12 - Concluindo Carrinho de Compras com TDD

--- src/Carrinho.php
+++ src/Carrinho.php
@@ -16,4 +16,20 @@ class Carrinho
     {
         return $this->produtos;
     }
+
+    public function getTotalProdutos()
+    {
+        return count($this->produtos);
+    }
+
+    public function getTotalCompra()
+    {
+        $totalCompra = 0;
+
+        foreach($this->produtos as $p) {
+            $totalCompra += $p->getPrice();
+        }
+
+        return $totalCompra;
+    }
 }
--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -36,4 +36,39 @@ class CarrinhoTest extends TestCase
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[0]);
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[1]);
     }
+
+    public function testSeValoresDeProdutosNoCarrinhoEstaoCorretosConformePassado()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice(19.99);
+        $produto->setSlug('produto-1');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+
+        $this->assertEquals('Produto 1', $carrinho->getProdutos()[0]->getName());
+        $this->assertEquals(19.99, $carrinho->getProdutos()[0]->getPrice());
+        $this->assertEquals('produto-1', $carrinho->getProdutos()[0]->getSlug());
+    }
+
+    public function testSeTotalDeProdutosEValorDaCompraEstaoCorretos()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice(19.99);
+        $produto->setSlug('produto-1');
+
+        $produto2 = new Produto();
+        $produto2->setName('Produto 2');
+        $produto2->setPrice(19.99);
+        $produto2->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+        $carrinho->addProduto($produto2);
+
+        $this->assertEquals(2, $carrinho->getTotalProdutos());
+        $this->assertEquals(39.98, $carrinho->getTotalCompra());
+    }
 }
